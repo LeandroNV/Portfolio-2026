103d1336ae7b74d91b57360730bc759b6dbb9909
Leandro <leonicvarc@gmail.com>
2026-02-27 10:35:30 -0500

    Initial Repo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from './sections/NavBar';
+
+export const App = () => {
+	return (
+		<div className='relative w-screen min-h-screen overflow-x-auto '>
+			<NavBar />
+		</div>
+	);
+};
---
+++ src/sections/NavBar.tsx
@@ -0,0 +1,14 @@
+import { useRef } from 'react';
+
+export const NavBar = () => {
+	const navRef = useRef(null);
+
+	return (
+		<nav
+			ref={navRef}
+			className='fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2 '
+		>
+			NavBar
+		</nav>
+	);
+};
